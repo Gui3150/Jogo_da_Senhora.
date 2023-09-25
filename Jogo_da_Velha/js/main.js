e4cb8ebc496b71cbc51a3c27f5473fdf97b2f4ef
Gui3150 <127741744+Gui3150@users.noreply.github.com>
2023-09-25 08:47:13 -0300

    Add files via upload

---
+++ Jogo_da_Velha/js/main.js
@@ -0,0 +1,13 @@
+// Criar variavéis no Javascript
+//var letra = "O"; não usar
+
+//const letra = 'D';
+//console.log(letra);
+
+
+let letra = "X";
+
+function joga(cel) {
+    let casaClicada = document.getElementById(cel).innerHTML;
+    
+}
