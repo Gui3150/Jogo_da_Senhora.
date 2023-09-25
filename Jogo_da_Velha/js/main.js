1dec70f1c32cc3114eee11a6b57be35b1a2995c2
Gui3150 <127741744+Gui3150@users.noreply.github.com>
2023-09-25 08:51:37 -0300

    Add files via upload

--- Jogo_da_Velha/js/main.js
+++ Jogo_da_Velha/js/main.js
@@ -9,5 +9,7 @@ let letra = "X";
 
 function joga(cel) {
     let casaClicada = document.getElementById(cel).innerHTML;
-    
+    if(casaClicada == "X" || casaClicada ='O') {
+        alert("Já foi escolhida essa posição");
+    }
 }
